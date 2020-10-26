86d28944ff274db1a10c1b723348f168ffc2cd32
Robert Coutts <blast5761@gmail.com>
2020-10-26 10:39:30 +0000

    Add functions for starting and stopping the client loop

--- client/clientgameloop.js
+++ client/clientgameloop.js
@@ -1,4 +1,20 @@
+var loopShouldRun = true;
+
 function loop(level) {
     level.update();
     render(level);
+
+    // Add other stuff here
+
+    if (loopShouldRun)
+    	window.requestAnimationFrame(() => {loop(level)});
+}
+
+function startLoop(level) {
+	loopShouldRun = true;
+	window.requestAnimationFrame(() => {loop(level)});
+}
+
+function stopLoop() {
+	loopShouldRun = false;
 }
