8319a74d598995836ef020304f140d98eb94c42a
mta2k00 <56701243+mta2k00@users.noreply.github.com>
2020-10-31 18:11:50 +0000

    Added some rotation code
    The player can now change sprites for rotate based on key inputs. However, I think something on the server side needs to be adapted otherwise the game crashes.

--- common/gameobjects.js
+++ common/gameobjects.js
@@ -23,25 +23,28 @@ class Player extends GameObject {
 		this.kills = 0;
 		this.points = 0;
 		this.zPos = 5;
+
+		this.holdingBall = false;
+		this.facing = "up";
 	}
-	
+
 	addToPage() {
 		if (objects.querySelector("#"+this.id) != undefined)
 			objects.removeChild(ui.querySelector("#"+this.id))
-			
+
 		var playerDiv = document.createElement("div");
 		playerDiv.setAttribute("id", "playerdiv");
 
 		//get player spritesheet
-		const playerImg = document.createElement("img");
+		var playerImg = document.createElement("img");
 		playerImg.setAttribute("id", "playerimg");
-		playerImg.setAttribute("src", "../client/assets/images/temp_player.png");
-		
+		playerImg.setAttribute("src", "../client/assets/images/player_up.png");
+
 		//set player size
 		playerDiv.style.width = "64px";
 		playerDiv.style.height = "64px";
 		playerDiv.style.zIndex = this.zPos;
-		
+
 		playerImg.style.width = "100%";
 		playerImg.style.height = "100%";
 
@@ -53,15 +56,72 @@ class Player extends GameObject {
 
 		playerDiv.appendChild(playerImg);
 		objects.appendChild(playerDiv);
+
+		//Add event listener to handle sprite changes
+		document.addEventListener("keydown", this.readKey);
 	}
-	
+
+
+    	readKey(key) {
+        	//check for left arrow key or A key press
+        	if((key.keyCode == 37) || (key.keyCode == 65)) {
+			//check that player is not already facing left
+			if(this.facing != "left") {
+				let playerImg = document.getElementById("playerimg");
+				playerImg.setAttribute("src", "../client/assets/images/player_left.png");
+				this.facing = "left";
+			}
+        	}
+
+        	//check for right arrow key or D key press
+        	if((key.keyCode == 39) || (key.keyCode == 68)) {
+			if(this.facing != "right") {
+				let playerImg = document.getElementById("playerimg");
+				playerImg.setAttribute("src", "../client/assets/images/player_right.png");
+				this.facing = "right";
+			}
+        	}
+
+        	//check for up arrow key or W key press
+        	if((key.keyCode == 38) || (key.keyCode == 87)) {
+			if(this.facing != "up") {
+				let playerImg = document.getElementById("playerimg");
+				playerImg.setAttribute("src", "../client/assets/images/player_up.png");
+				this.facing = "up";
+			}
+        	}
+
+        	//check for down arrow key or S key press
+        	if((key.keyCode == 40) || (key.keyCode == 83)) {
+			if(this.facing != "down") {
+				let playerImg = document.getElementById("playerimg");
+				playerImg.setAttribute("src", "../client/assets/images/player_down.png");
+				this.facing = "down";
+			}
+        	}
+    	}
+
+        //check for spacebar pressed (attack button)
+		//TODO: Finish off this commented out code for player attack
+		/*if(key.keyCode == 32) {
+            if(this.holdingBall){
+                //code to drop ball
+                this.holdingBall = false;
+            }
+            console.log("Attack!"); //Test code
+        }
+		*/
+
+	changeSprite(spriteDirLocation) {
+		playerImg.setAttribute("src", spriteDirLocation);
+	}
+
 	updateStats(wins, kills, points){
 		this.wins += wins;
 		this.kills += kills;
 		this.points += points;
 	}
 }
-
 class Point extends GameObject {
 	constructor(pos, rotation, level) {
 		this.velocity = velocity;
