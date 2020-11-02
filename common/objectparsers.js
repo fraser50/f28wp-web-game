109fef90cfcf7914d757653715b6167a44a1052d
Fraser Watson <fraser.watson@mac.com>
2020-11-02 16:49:49 +0000

    Added object parsing code

---
+++ common/objectparsers.js
@@ -0,0 +1,34 @@
+gameobjectgenerators = [
+    [Player, function(jobj, level) {
+        return new Player(new Position(jobj["x"], jobj["y"]), 0, level, new Vector(0, 0), 0);
+
+    }],
+
+    [Point, function(jobj, level) {
+        return new Point(new Position(jobj["x"], jobj["y"]), 0, level);
+    }]
+]
+
+function objToJSON(gobj) {
+    var jsonobj = gobj.toJSON();
+
+    for (var i = 0; i < gameobjectgenerators.length; i++) {
+        if (gobj instanceof gameobjectgenerators[i][0]) {
+            jsonobj["type"] = i;
+            return jsonobj;
+        }
+    }
+}
+
+function objFromJSON(jsonobj, level) {
+    var typeindex = jsonobj["type"];
+
+    if (typeindex >= gameobjectgenerators.length) {
+        throw new Error("Invalid object type");
+    }
+
+    var generator = gameobjectgenerators[typeindex][1];
+
+    return generator(jsonobj, level);
+
+}
