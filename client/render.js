12c2f2888b813ba59a40a497fb188186de9468e5
Fraser Watson <fraser.watson@mac.com>
2020-11-10 20:18:49 +0000

    Render code for Point

--- client/render.js
+++ client/render.js
@@ -16,9 +16,14 @@ elementbuilders = [ // A new function will be placed here for creating the eleme
 
 	[Point, function(obj) {
 		var e = document.createElement("img");
+
+		e.src = "client/assets/images/Ball.png";
+		e.width = zoomLevel + "px";
+		e.height = zoomLevel + "px";
+		elem.style.zIndex = "4";
 		e.style.top = 0;
 		e.style.left = 0;
-		e.src = "client/assets/images/point.png";
+
 		return e;
 	}]
 ]
